3358f6d4fc622528bde956f6c34c29821b86bccc
platoo <lacheephyo@gmail.com>
2017-01-18 17:36:55 -0500

    Whatever that I could finish today admists a hundred meetings and other ad hoc work

--- data_architect_misc/ftp_scan.py
+++ data_architect_misc/ftp_scan.py
@@ -1,8 +1,40 @@
-import pdb
+"""
+Script to scan the data files in FTP location and produce metadata files
+for those that don't have one already. The requirements is like this:
 
+Data File Name:
+DCM_Campaign_Report_636130080295384737_wxr45umjgmn_10252016.zip
+
+Control File Name:
+DCM_Campaign_Report_636130080295384737_wxr45umjgmn_control_file_10252016.csv
+
+Example of columns within the control file (comma delimited):
+File_name,My_test_file_name.zip
+Zipped_file_size,2345
+Unzipped_file_size,658564
+Row_count,30
+Column_count,10
+"""
+
+import pdb
+import re
 from ftplib import FTP
 
 from account_info import *
 
-ftp = FTP(user=USR_OUTBOUND, passwd=PWD_OUTBOUND)
+metafile_keyword = 'control_file'
 
+with FTP(FTP_HOST) as ftp:
+    ftp.login(user = USR_OUTBOUND, passwd = PWD_OUTBOUND)
+    ftp.cwd(ROOT_OUTBOUND)
+    for dir in ftp.nlst():
+        ftp.cwd('/'.join((ROOT_OUTBOUND, dir)))
+
+        files = []
+        for f in ftp.nlst():
+            re.match(metafile_keyword, f, re.M|re.I)
+            print(f)
+
+
+
+print('haha')
