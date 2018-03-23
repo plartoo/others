d90a1ff038c622180118bf725b3740410a802669
plartoo <lacheephyo@gmail.com>
2018-03-23 00:27:24 -0400

    Minor changes like var name and using pprint to inspect content

--- data_architect_misc/kf_generator/generate_kf.py
+++ data_architect_misc/kf_generator/generate_kf.py
@@ -4,13 +4,13 @@ import pdb
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
-from account_info import SERVER_INFO_STR
-from sql_server_utils import SqlServerUtils
+import account_info
 import queries
+from sql_server_utils import SqlServerUtils
 
 def get_country_metadata():
-    db = SqlServerUtils(DM_1219)
-    data = db.fetch_all_data(queries.load_country_metadata)
+    db = SqlServerUtils(account_info.DM_1219)
+    data = db.fetch_all_data(queries.country_metadata)
     return {row[1]: dict(zip(data[0], row)) for row in data[1:]}
 
 
@@ -30,6 +30,7 @@ if __name__ == '__main__':
         if c in country_metadata:
             print('\nGenerating fact file(s) for this country:', c)
             c_metadata = country_metadata[c]
+            pp.pprint(c_metadata)
         else:
             print('\n**** WARNING: country name is not found in our metadata table =>', c)
 
