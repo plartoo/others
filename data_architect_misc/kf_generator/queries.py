c76933a572b2850931d472d7eb080ab198b6cbdb
plartoo <lacheephyo@gmail.com>
2018-03-24 18:35:52 -0400

    Updated some and added some more.

--- data_architect_misc/kf_generator/queries.py
+++ data_architect_misc/kf_generator/queries.py
@@ -1,9 +1,5 @@
 country_metadata = """
-SELECT 
-    [CP_COUNTRY_ID],[COUNTRY_KEY],[COUNTRY_NAME],[GM_COUNTRY_ID],
-    [HUB_ID],[HUB_KEY],[HUB_TEXT],[SUBDIVISION_ID],[SUBDIVISION_KEY],
-    [SUBDIVISION_TEXT],[DIVISION_ID],[DIVISION_KEY],[DIVISION_TEXT],
-    [ADD_DATE] 
+SELECT *
 FROM [DM_1219_ColgateGlobal].[dbo].[CP_DIM_COUNTRY]
 """
 
@@ -12,7 +8,7 @@ SELECT
     [Demographics], [TA Dim Text], [Dim Type], [Dim Type Text], 
     [Is Gbl TA? Key], [Is Gbl TA? Text], [Global Name], 
     [Global Name Text], [Sort Number] 
-FROM [dbo].[V_MED_DEMO_DM]
+FROM [DM_1219_ColgateGlobal].[dbo].[V_MED_DEMO_DM]
 """
 
 geography = """
@@ -21,7 +17,7 @@ SELECT
     [Division], [Subdivision], [Hub], [Country], [Region], 
     [Region Text], [SubRegion], [SubRegion Text], [Province], 
     [ProvinceText], [CityTier], [CityTier Text], [City], [City Text] 
-FROM [dbo].[V_MED_GEO_DM]
+FROM [DM_1219_ColgateGlobal].[dbo].[V_MED_GEO_DM]
 """
 
 product = """
@@ -30,7 +26,7 @@ SELECT
     [6L Category], [6L Subcategory], [6L Brand], [6L Subbrand], 
     [6L Variant], [1PH Category], [1PH Subcategory], [1PH ProductCategory], 
     [1PH Brand], [1PH Subbrand], [1PH Variant] 
-FROM [dbo].[V_MED_PROD_DM]
+FROM [DM_1219_ColgateGlobal].[dbo].[V_MED_PROD_DM]
 """
 
 media = """
@@ -38,14 +34,14 @@ SELECT
     [Media Dim], [Media Dim Text], [Dim Type], [Dim Type Text], 
     [Media lvl 1], [Media lvl 1 text], [Media lvl 2], [Media lvl 2 text], 
     [Media lvl 3], [Media lvl 3 text], [Spot Length], [Spot  Bucket Length] 
-FROM [dbo].[V_MED_MEDIA_DM]
+FROM [DM_1219_ColgateGlobal].[dbo].[V_MED_MEDIA_DM]
 """
 
 creative = """
 SELECT 
     [Creative Dim], [Creative Dim text], [Dim Type], [Dim Type Text], 
     [Global Name], [Global Name Text] 
-FROM [dbo].[V_MED_CREA_DM]
+FROM [DM_1219_ColgateGlobal].[dbo].[V_MED_CREA_DM]
 """
 
 network = """
@@ -64,13 +60,17 @@ brand = """
 SELECT 
     [Country ID], [CountryName], [BrandID], [BrandName], 
     [LocalManuf], [GlobalManuf], [StartDate] 
-FROM [dbo].[V_MED_BRAND_MANF]
+FROM [DM_1219_ColgateGlobal].[dbo].[V_MED_BRAND_MANF]
 """
 
+# TODO: Fix incorrect format for country_id and subcat_key columns in our db
 market_subcat = """
 SELECT 
-    [Country ID], [Subcategory Key], [C/NC Flag], [C/NC Text] 
-FROM [dbo].[V_MED_MARKET_SUBCAT]
+    CAST([Country ID] AS INT) AS [Country ID], 
+    CAST([Subcategory Key] AS INT) AS [Subcategory Key],
+    [C/NC Flag],
+    [C/NC Text]
+FROM [DM_1219_ColgateGlobal].[dbo].[V_MED_MARKET_SUBCAT]
 """
 
 # TODO:
@@ -79,24 +79,25 @@ Jholman will reate this view later
 """
 
 dim_queries = [
-    {'MED_DEMO_DM_ALL': demographic, 'split_file': False},
-    {'MED_GEO_DM_ALL': geography, 'split_file': False},
-    {'MED_PROD_DM_ALL': product, 'split_file': False},
-    {'MED_MEDIA_DM_ALL': media, 'split_file': False},
-    {'MED_CREA_DM_ALL': creative, 'split_file': False},
-    {'MED_NETWORK_DM_ALL': network, 'split_file': False},
-    {'MED_DAYPART_DM_ALL': daypart, 'split_file': False},
-    {'MED_BRAND_MANUF_ALL': brand, 'split_file': False},
-    {'MED_MARKET_SUBCAT_ALL': market_subcat, 'split_file': False},
+    {'query': demographic, 'output_file_basename': 'MED_DEMO_DM_ALL'},
+    {'query': geography, 'output_file_basename': 'MED_GEO_DM_ALL'},
+    {'query': product, 'output_file_basename': 'MED_PROD_DM_ALL'},
+    {'query': media, 'output_file_basename': 'MED_MEDIA_DM_ALL'},
+    {'query': creative, 'output_file_basename': 'MED_CREA_DM_ALL'},
+    {'query': network, 'output_file_basename': 'MED_NETWORK_DM_ALL'},
+    {'query': daypart, 'output_file_basename': 'MED_DAYPART_DM_ALL'},
+    {'query': brand, 'output_file_basename': 'MED_BRAND_MANUF_ALL'},
+    {'query': market_subcat, 'output_file_basename': 'MED_MARKET_SUBCAT_ALL'},
     # {'MED_CHANNEL_DM_ALL':, 'split_file': False}, # TODO: add after Jholman created channel view
 ]
+
 
 def get_fact_query(country_metadata):
     fact_query_non_APAC = """
     SELECT	[Geography Dim], [Product Dim], [Media Dim], [Demographic Dim], [Creative Dim], 
     		[Daypart Dim], [Network Dim], [Month Year], [Country ID], [Local Currency], 
     		[Spend Local], [Spend USD], [TRP], [Normalized TRP], [Insertions], [Impressions] 
-    FROM [dbo].[V_Transaction_Data] 
+    FROM [DM_1219_ColgateGlobal].[dbo].[V_Transaction_Data] 
     WHERE [COUNTRY ID] = @CP_COUNTRY_ID 
     """
 
@@ -104,13 +105,15 @@ def get_fact_query(country_metadata):
     SELECT	[Geography Dim], [Product Dim], [Media Dim], [Demographic Dim], [Creative Dim], 
     		[Daypart Dim], [Network Dim], [Month/Year] AS [Month Year], [Country] AS [Country ID], 
     		[Local Currency], [Spend Local], [Spend USD], [TRP], [Normalized TRP], [Insertions],[Impressions]  
-    FROM [dbo].[MED_KF_@MERGED_COUNTRY_KEY] 
+    FROM [DM_1035_ColgateAPACCompetitive].[dbo].[MED_KF_@MERGED_COUNTRY_KEY] 
     """
 
-    query = None
-    if country_metadata['DIVISION_TEXT'] == 'Asia':
-        query = fact_query_APAC.replace('@MERGED_COUNTRY_KEY', country_metadata['MERGED_COUNTRY_KEY'])
+    output_file = 'MED_KF_'
+    if country_metadata['division_text'] == 'Asia':
+        query = fact_query_APAC.replace('@MERGED_COUNTRY_KEY', str(country_metadata['merged_country_key']))
+        output_file += str(country_metadata['merged_country_key'])
     else:
-        query = fact_query_non_APAC.replace('@CP_COUNTRY_ID', country_metadata['CP_COUNTRY_ID'])
+        query = fact_query_non_APAC.replace('@CP_COUNTRY_ID', str(country_metadata['cp_country_id']))
+        output_file += str(country_metadata['country_key'])
 
-    return {'MED_KF_': query, 'split_file': True, 'by_row_count': {}
+    return {'query': query, 'output_file_basename': output_file}
