643a6098187ff333df79aff323f3cfb280f7b630
plartoo <lacheephyo@gmail.com>
2018-03-25 17:57:29 -0400

    Added a new method to incrementally write data.

--- data_architect_misc/kf_generator/data_writer.py
+++ data_architect_misc/kf_generator/data_writer.py
@@ -23,13 +23,14 @@ class DataWriter(object):
             os.makedirs(self.output_dir)
 
     def get_size_in_bytes(self, row):
+        # Note: this will only return the best size approximation for each row
         qc = self.quotechar # workaround for REF: https://stackoverflow.com/a/28130950
         row_str = self.delimiter.join((qc+str(r)+qc) for r in row) #+ self.lineterminator
         # REF1: https://stackoverflow.com/a/4013418
         # REF2: https://stackoverflow.com/q/5290182
         return len(row_str.encode(self.encoding))
 
-    def write_to_file(self, data, configs):
+    def write_to_file(self, data, output_file, configs):
         self.encoding = configs['encoding'] if 'encoding' in configs else self.encoding
         self.newline = configs['newline'] if 'newline_char' in configs else self.newline
         self.lineterminator = configs['lineterminator'] if 'lineterminator' in configs else self.lineterminator
@@ -37,7 +38,6 @@ class DataWriter(object):
         self.quotechar = configs['quotechar'] if 'quotechar' in configs else self.quotechar
         self.quoting = configs['quoting'] if 'quoting' in configs else self.quoting
 
-        output_file = configs['output_file']
         file_extension = '.tsv' if self.delimiter == '\t' else '.csv'
         output_file = ''.join([output_file, file_extension])
         with open(output_file, 'w', newline=self.newline, encoding=self.encoding) as fo:
@@ -50,21 +50,23 @@ class DataWriter(object):
                 print('\n\n***!!! Error in writing CSV (output) file: ', output_file, '\n\n')
                 sys.exit()
 
-    def write_data(self, data, configs):
+    def prepare_output_file_path_and_name(self, file_num, file_basename):
+        return file_basename + str(file_num) + datetime.datetime.now().strftime('_%m_%Y')
+
+    def write_data_all_at_once(self, data, configs):
         include_header = configs['include_header'] if 'include_header' in configs else True
         split = configs['split_file'] if 'split_file' in configs else False
         split_by = configs['split_by'] if 'split_by' in configs else 'row'
         split_limit = configs['split_limit'] if 'split_limit' in configs else self.row_count_limit
 
         if not split:
-            configs['output_file'] = os.path.join(self.output_dir, configs['output_file_basename'])
-            self.write_to_file(data, configs)
+            self.write_to_file(data, os.path.join(self.output_dir, configs['output_file_basename']), configs)
             print('<===\n')
         else:
             # I can split data using list comprehension like below and write it. The code would be much shorter/cleaner!
             # REF: https://stackoverflow.com/a/312464
             # [data[i:i+n] for i in range(0,len(data),n)]
-            # But that may create space inefficiencies for gigantic lists. TODO: verify space inefficiencies
+            # But that may create space inefficiencies for gigantic lists.
             # That's why I opted instead to write chunk by chunk (looks a bit naive to say the least)
             cur_row_num = 0
             file_count = 1
@@ -80,10 +82,9 @@ class DataWriter(object):
                     cur_doc.append(row)
                     cur_doc_size += cur_row_size
                 else:
-                    output_file_name = configs['output_file_basename'] + str(file_count)\
-                                             + datetime.datetime.now().strftime('_%m_%Y')
-                    configs['output_file'] =  os.path.join(self.output_dir, output_file_name)
-                    self.write_to_file(cur_doc, configs)
+                    output_file_name = self.prepare_output_file_path_and_name(str(file_count),
+                                                                              configs['output_file_basename'])
+                    self.write_to_file(cur_doc, os.path.join(self.output_dir, output_file_name), configs)
                     print('at row number:', str(i+1), '\t\twith approx. file size (in MB):',
                           cur_doc_size/DataWriter.BYTES_IN_MEGABYTES)
                     file_count += 1
@@ -92,9 +93,99 @@ class DataWriter(object):
                     cur_doc = [header] if include_header else []
                     cur_doc_size = header_size if include_header else 0
 
-            output_file_name = configs['output_file_basename'] + str(file_count) \
-                               + datetime.datetime.now().strftime('_%m_%Y')
-            configs['output_file'] = os.path.join(self.output_dir, output_file_name)
-            self.write_to_file(cur_doc, configs)
+            output_file_name = self.prepare_output_file_path_and_name(str(file_count),
+                                                                      configs['output_file_basename'])
+            self.write_to_file(cur_doc, os.path.join(self.output_dir, output_file_name), configs)
             print('at row number:', str(cur_row_num+1), '\t\twith approx. file size (in MB):',
                   cur_doc_size/DataWriter.BYTES_IN_MEGABYTES, '\n<===\n')
+
+    def get_size_of_first_row(self, db_connection, query):
+        cursor = db_connection.cursor()
+        cursor.execute(query)
+        row = [i for i in cursor.fetchone()]
+        cursor.close()
+        return self.get_size_in_bytes(row)
+
+    def get_approximate_rows_per_doc(self, row_size, size_limit):
+        return int(size_limit/row_size)
+
+    def get_row_per_doc(self, db_connection, configs):
+        # since we are going to pull and write data incrementally, we need two parameters below
+        if ('split_by' not in configs) or ('split_limit' not in configs):
+            sys.exit("\n\n***!!! You must define how to ('split_by') and when to ('split_limit') "
+                     "split the data if you are to incrementally write data."
+                     "\nTerminating the program until these parameters are included in configuration.\n")
+
+        if configs['split_by'] == 'row':
+            print("Max. rows per doc:", str(configs['split_limit']))
+            return configs['split_limit']
+        elif configs['split_by'] == 'size':
+            row_size = self.get_size_of_first_row(db_connection, configs['query'])
+            row_limit = self.get_approximate_rows_per_doc(row_size, configs['split_limit'])
+            print("Max. rows per doc:", str(row_limit), "\t\twith approx. file size (in MB):",
+                  str(row_size*row_limit/DataWriter.BYTES_IN_MEGABYTES))
+            return self.get_approximate_rows_per_doc(row_size, configs['split_limit'])
+        else:
+            print("Max. rows per doc:", str(self.row_count_limit))
+            return self.row_count_limit
+
+    def write_data_incrementally(self, db_connection, configs):
+        include_header = configs['include_header'] if 'include_header' in configs else True
+        row_per_doc = self.get_row_per_doc(db_connection, configs)
+
+        cursor = db_connection.cursor()
+        cursor.execute(configs['query'])
+
+        file_count = 1
+        total_row_count = 0
+        header = [header[0] for header in cursor.description]
+        print("Row per doc:", str(row_per_doc))
+        while True:
+            # Here, I decided to use 'fetchmany' instead of 'fetchone' (see footnote for detail)
+            # REF: https://github.com/mkleehammer/pyodbc/wiki/Cursor
+            rows = [list(row) for row in cursor.fetchmany(row_per_doc)]
+            if not rows:
+                break
+
+            cur_doc = ([header] + rows) if include_header else rows
+            total_row_count += len(rows)
+            output_file_name = self.prepare_output_file_path_and_name(str(file_count),
+                                                                      configs['output_file_basename'])
+            self.write_to_file(cur_doc, os.path.join(self.output_dir, output_file_name), configs)
+            print('at row number:', str(total_row_count))
+            file_count += 1
+
+        cursor.close()
+
+
+    ## Footnote
+    # The incremental write method (write_data_incrementally) came about because some of our tables
+    # have tens of millions of rows. That pretty much ensured getting MemoryError from Python if
+    # we use 'write_data_all_at_once' because that approach loads ALL of the data in the table
+    # into Python's working memory.
+    #
+    # In incremental approach, we can go like this: fetch ONE row, then write that row to CSV,
+    # keep doing the previous two steps until no more rows to fetch. BUT that, in my opinion,
+    # is a waste because we can reduce the network IO (back and forth 'talking' between DB server
+    # and current Python instance) by fetching many rows per cursor's move forward. That's why
+    # I've decided to use 'fetchmany' method to fetch and write X (where X > 1) number of rows
+    # at every iteration/increment.
+    #
+    # Now, there comes the question of how to decide the optimal number of rows per fetch.
+    # Well...we have client's delivery requirement of 1.5 million rows per doc.
+    # And we also know that sometimes our FTPs aren't good enough to transfer files that are
+    # of in 100s of MB range. Using that as baseline, I've decide to roughly calculate the number
+    # of rows, which can be configured, to write per document using 'get_row_per_doc' method, which
+    # uses a simple formula (max_file_size_allowed/size_of_each_row) to estimate the number of
+    # rows per file (if row limit is not provided).
+    #
+    # Below is another way to calculate the number of rows per file if file size limit is given:
+    # 1. Pass table name and change the way we connect to DB (APAC vs. other)
+    # 2. Run cursor.columns(table=<table_name>) and get max possible size of each column like below:
+    # [i[6] for i in [r for r in cursor.columns(table='CP_DIM_COUNTRY')]]
+    # 3. Calculate number of rows that will fit in a split_limit based on info from #2 above
+    # This approach will tend to over-estimate the actual size of the rows under split_limit,
+    # so we may undershoot the number of rows in each file delivered.
+    #
+    # For now, let's stick to the naive approach of calculating row limit for each file. We can try
+    # the more robust approach later.
