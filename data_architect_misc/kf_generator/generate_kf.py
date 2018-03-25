cbdb8df8efde1cc7dbcac1022171ef8d38b0484f
plartoo <lacheephyo@gmail.com>
2018-03-25 17:58:25 -0400

    Split fact and dimension writing to two parts, each using different way of writing file (incremental vs. all_at_once)

--- data_architect_misc/kf_generator/generate_kf.py
+++ data_architect_misc/kf_generator/generate_kf.py
@@ -20,16 +20,13 @@ def prepare_fact_queries_and_output_configs(countries, country_metadata):
     q = []
     for c in countries:
         if c in country_metadata:
-            # pp.pprint(country_metadata[c])
             fact_query_and_output_config = queries.get_fact_query(country_metadata[c])
             fact_query_and_output_config['split_file'] = True
             fact_query_and_output_config['split_by'] = 'row' # alternative is 'size'
             fact_query_and_output_config['split_limit'] = ROW_COUNT # for 'size', provide integer as file size in MB
-            # pp.pprint(query_and_output_file_config)
             q.append(fact_query_and_output_config)
         else:
-            print('\n***!!! WARNING: [COUNTRY_KEY] is not found in the [CP_DIM_COUNTRY] =>', c)
-            sys.exit()
+            sys.exit('\n***!!! WARNING: [COUNTRY_KEY] is not found in the [CP_DIM_COUNTRY] => ' + c)
     return q
 
 
@@ -46,18 +43,20 @@ if __name__ == '__main__':
     print('List of countries provided: ', countries)
 
     country_metadata = get_country_metadata()
-    fact_and_dim_queries = prepare_fact_queries_and_output_configs(countries, country_metadata) \
-                            + queries.dim_queries
-
-    # print('\nGenerating fact file(s) for this country:', c)
+    fact_queries = prepare_fact_queries_and_output_configs(countries, country_metadata)
     print("\n===> List of queries and their configurations to be run <===")
-    pp.pprint(fact_and_dim_queries)
+    pp.pprint(fact_queries + queries.dim_queries)
+
     writer = DataWriter()
     db = SqlServerUtils(account_info.DM_1219)
-    for q in fact_and_dim_queries:
-        print("\n===> Running the following query with configuration below to generate the output file:")
+
+    for q in fact_queries:
+        print("\n===> Generating FACT file using the query and config below:")
         pp.pprint(q)
-        writer.write_data(db.fetch_all_data(q['query']), q)
+        writer.write_data_incrementally(db.get_connection(), q)
 
-
+    # for q in queries.dim_queries:
+    #     print("\n===> Generating Dimension file using the query and config below:")
+    #     pp.pprint(q)
+    #     writer.write_data_all_at_once(db.fetch_all_data(q['query']), q)
     print('\n###Key figure file generation completed.###')
