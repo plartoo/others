1f281760878615f8ed56d31fc0782b3a3c7bfda0
plartoo <lacheephyo@gmail.com>
2018-03-30 01:25:04 -0400

    Still figuring out how to stop getting junk content from rogue sites

--- data_architect_misc/adstxt/adstxt/spiders/adstxt.py
+++ data_architect_misc/adstxt/adstxt/spiders/adstxt.py
@@ -14,6 +14,7 @@ class AdsTxtSpider(scrapy.Spider):
     COMMENT_CHAR = '#'
     DELIMITER = ','
     OUTPUT_DIR = None
+    # _stop_following_links = True # TODO: remove. Does NOT work
 
     @classmethod
     def create_output_dir(cls):
@@ -25,7 +26,7 @@ class AdsTxtSpider(scrapy.Spider):
     # TODO: replace thie method with loader for S3/Redshift
     def load_urls_to_crawl(self):
         cur_dir_path = os.path.dirname(os.path.realpath(__file__))
-        url_file = os.path.join(cur_dir_path, 'urls_to_scrape.csv')
+        url_file = os.path.join(cur_dir_path, 'urls_to_scrape_2.csv')
         with open(url_file, 'r') as csvfile:
             csvreader = csv.reader(csvfile)
             return [''.join(['https://',r[0],'/ads.txt']) for r in csvreader][0:10000]
@@ -33,24 +34,34 @@ class AdsTxtSpider(scrapy.Spider):
     def start_requests(self):
         urls = self.load_urls_to_crawl()
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            print('start:', url)
+            # Alternative way to stop redirects: https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#module-scrapy.downloadermiddlewares.redirect
+            yield scrapy.Request(url=url, callback=self.parse, meta={'dont_merge_cookies': True})
 
     def parse_ads_txt_line(self, ads_txt_str):
         field_names = ['exchange_name', 'exchange_id', 'payment_type', 'tag_id']
         return dict(itertools.zip_longest(field_names, [i.strip() for i in ads_txt_str.split(AdsTxtSpider.DELIMITER)]))
 
     def parse(self, response):
-        cur_datetime = datetime.now().strftime("%Y-%m-%d %H:%M")
-        for line in response.body.split(b'\n'):
-            # Decode from str to bytes so that the carriage returns can be stripped away
-            split_line = line.decode('utf-8').split(AdsTxtSpider.COMMENT_CHAR)
-            comment = ''.join(split_line[1:]).strip()
-            if split_line[0].strip():
-                ads_txt_dict = self.parse_ads_txt_line(split_line[0])
-                ads_txt_dict['has_adstxt'] = 1
-                # REF: https://stackoverflow.com/a/20728813
-                ads_txt_dict['domain'] = "{0.scheme}://{0.netloc}/".format(urlsplit(response.url))
-                ads_txt_dict['ads_txt_url'] = response.url
-                ads_txt_dict['last_fetched_date'] = cur_datetime
-                ads_txt_dict['comment'] = comment
-                yield ads_txt_dict
+        if '/ads.txt' in response.url:
+        # if response.url != :
+            print(response.url, ':', response.status)
+            cur_datetime = datetime.now().strftime("%Y-%m-%d %H:%M")
+            for line in response.body.split(b'\n'):
+                # Decode from str to bytes so that the carriage returns can be stripped away
+                split_line = line.decode('utf-8').split(AdsTxtSpider.COMMENT_CHAR)
+                comment = ''.join(split_line[1:]).strip()
+                if split_line[0].strip():
+                    ads_txt_dict = self.parse_ads_txt_line(split_line[0])
+                    ads_txt_dict['has_adstxt'] = 1
+                    # REF: https://stackoverflow.com/a/20728813
+                    ads_txt_dict['domain'] = "{0.scheme}://{0.netloc}/".format(urlsplit(response.url))
+                    ads_txt_dict['ads_txt_url'] = response.url
+                    ads_txt_dict['last_fetched_date'] = cur_datetime
+                    ads_txt_dict['comment'] = comment
+                    yield ads_txt_dict
+        else: #TODO: remove this because it creates empty rows in CSV file
+            print('SKIPPED:', response.url, ':', response.status)
+            yield {}
+
+# TODO: Add RetryMiddleware: https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#module-scrapy.downloadermiddlewares.retry
