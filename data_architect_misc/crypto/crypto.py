674577bf10c77828fafe8d53e7d8ba7691a1f39d
platoo <lacheephyo@gmail.com>
2018-04-16 16:18:30 -0400

    Removed testing parts and modified to allow salt file genration

--- data_architect_misc/crypto/crypto.py
+++ data_architect_misc/crypto/crypto.py
@@ -1,3 +1,4 @@
+import argparse
 import base64
 import os
 
@@ -7,15 +8,11 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 class Crypto(object):
-    ENCODING = 'utf-8'
     CUR_DIR = os.path.dirname(os.path.realpath(__file__))
-    ENCRYPTED_FILE_NAME = 'encrypted_data'
     SALT_FILE_NAME = 'salt'
-    ENCRYPTED_FILE = os.path.join(CUR_DIR, ENCRYPTED_FILE_NAME)
-    SALT_FILE = os.path.join(CUR_DIR, SALT_FILE_NAME)
 
 
-    def __load_salt(self, salt_file=SALT_FILE):
+    def __load_salt(self, salt_file):
         with open(salt_file, 'rb') as fi:
             return fi.read()
 
@@ -25,7 +22,7 @@ class Crypto(object):
         return os.urandom(16)
 
 
-    def generate_salt_file(self, dir=CUR_DIR, fname=SALT_FILE_NAME):
+    def generate_salt_file(self, dir, fname):
         salt_file = os.path.join(dir, fname)
         with open(salt_file, 'wb') as fo:
             fo.write(self.__generate_salt())
@@ -50,48 +47,31 @@ class Crypto(object):
         return base64.urlsafe_b64encode(kdf.derive(password))
 
 
-    def encrypt_and_write(self, data, password, salt_file=SALT_FILE, outfile=ENCRYPTED_FILE):
+    def encrypt_and_write(self, data, password, salt_file, outfile):
         fernet = Fernet(self.generate_key(password, salt_file))
         with open(outfile, 'wb') as fo:
             fo.write(fernet.encrypt(data))
         print("Encrypted data written to:", outfile)
 
 
-    def load_and_decrypt(self, encrypted_file, outfile, password, salt_file=SALT_FILE):
+    def load_and_decrypt(self, encrypted_file, outfile, password, salt_file):
         fernet = Fernet(self.generate_key(password, salt_file))
         with open(encrypted_file, 'rb') as fi:
             encrypted_data = b''.join(l for l in fi.readlines())
             data = fernet.decrypt(encrypted_data)
             with open(outfile, 'wb') as fo:
                 fo.write(data)
+                print("Decrypted data written to:", outfile)
 
 
 if __name__ == '__main__':
-    c = Crypto()
-    # with open(os.path.join('1_raw','test_csv.csv'), 'r') as fi:
-    #     data = b''.join(l.encode(c.ENCODING) for l in fi.readlines())
-    #     c.encrypt_and_write(data, b'123')
-    # c.generate_salt_file()
+    parser = argparse.ArgumentParser(description='To generate salt file, use -s flag')
+    parser.add_argument('-s',
+                        default=0,
+                        type=int,
+                        help="If '1', this will generate salt file in the same directory as the code is")
+    args = parser.parse_args()
 
-    # with open(os.path.join('1_raw','test_csv.csv'), 'rb') as fi:
-    #     c.encrypt_and_write(fi.read(), b'123')
-    # load_and_decrypt('encrypted_data', 'decrypted_csv.csv', b'123')
-
-    # with open(os.path.join('1_raw','test_xls.xls'), 'rb') as fi:
-    #     c.encrypt_and_write(fi.read(), b'123')
-    # c.load_and_decrypt('encrypted_data', 'decrypted_xls.xls', b'123')
-
-    # with open(os.path.join('1_raw','test_xlsx.xlsx'), 'rb') as fi:
-    #     c.encrypt_and_write(fi.read(), b'123')
-    # c.load_and_decrypt('encrypted_data', 'decrypted_xlsx.xlsx', b'123')
-
-
-    # create_folder
-    # move_file
-    # delete_file
-    # delete_folder
-
-    print("Done")
-
-# https://stackoverflow.com/questions/9884353/xls-to-csv-converter
-# https://gist.github.com/scottming/99c09685360376d4cac2de7c891e8050/9670acd5044d11871188e04b8123263ad132f512
+    if args.s:
+        c = Crypto()
+        c.generate_salt_file(c.CUR_DIR, c.SALT_FILE_NAME)
