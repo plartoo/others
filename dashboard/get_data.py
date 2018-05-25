8008abb881e6a938c321217f25d9d7c337d39f2f
plartoo <lacheephyo@gmail.com>
2018-05-25 00:34:10 -0400

    Unfinished business. Still need to figure out how to nest nodes properly

--- dashboard/get_data.py
+++ dashboard/get_data.py
@@ -15,51 +15,83 @@ ROOT = 'ALL'
 ANCHOR = 'Year'
 METRICS = ['SumOfSpend','UniqBrandCount','UniqRegionCount','UniqCountryCount','UniqAdvertiserCount']
 
-def build_data_block(row, level, label=None):
+def build_data_block(row, level, hierarchy, label=None):
     # print(row)
     # pdb.set_trace()
     # print(row)
     # print(level)
-    name = ROOT if level == ROOT else row[level]
-    return {
+    name = ROOT if level in (ROOT, ANCHOR) else row[level]
+    d = {
         'name': name,
         'value': [row[m] for m in METRICS],
         'label': label
     }
+    d.setdefault('children',[]) if hierarchy[-1] != level
+    return d
 
 def build_data_tree_by_year(dataframe, hierarchy):
     assert ANCHOR in dataframe.columns, \
         "'build_data_tree_by_year' expects the input dataframe to have '" + ANCHOR + "' column"
+    assert ANCHOR == hierarchy[0], \
+        "'build_data_tree_by_year' expects the first item in hierarchy as '" + ANCHOR + "'"
     assert len(hierarchy) > 1, "hierarchy list should have at least two items"
 
     # ['ALL','1 nonempty','2 nonempty','3 nonempty',...] #=> '1 nonempty' is where all data that have regional aggregate goes to etc.
     #
     # ['ALL','2013','2014','2015','2016']
 
-    hierarchy = [ROOT] + hierarchy
-    # dataframe[[ANCHOR]] = dataframe[[ANCHOR]].fillna(value=0)#.astype(int).astype(str)
-    years = [x if x else ROOT for x in dataframe.Year.unique()]
-    tree = {y: [] for y in years}
-    temp = {y: {h: [] for h in hierarchy} for y in years}
+    temp = {ROOT:[]}
     i = 0
     for index, row in dataframe.iterrows():
         i += 1
-        for prev,cur in zip(hierarchy, hierarchy[1:]):
-            print(prev,cur)
-            if row[cur] is not None:
-                continue
-            else:
-                k = ROOT if row[ANCHOR] is None else row[ANCHOR]
-                temp[k][prev].append(build_data_block(row, prev))
-                break
-
-        if i == 13:
-            pp.pprint(temp['2013'])
+        cur_row = [row[h] for h in hierarchy]
+        nonempty_field_cnt = sum(x is not None for x in cur_row)
+        level = ROOT if nonempty_field_cnt == 0 else hierarchy[nonempty_field_cnt-1]
+        block = build_data_block(row, level)
+        if level == ROOT:#nonempty_field_cnt == 0:
+            temp[ROOT].append(block)
+        elif level == ANCHOR:
+            yr = row[ANCHOR]
+            temp.setdefault(yr, [])
+            temp[yr].append(block)
             pdb.set_trace()
-            print('halo')
+            print('ha')
+        else:
+            for h in ([ROOT] + hierarchy[:(nonempty_field_cnt-1)]):
 
-    pdb.set_trace()
-    print('ha')
+
+
+
+
+
+        print(r, ':', none_count)
+        if i > 14:
+            break
+
+    # hierarchy = [ROOT] + hierarchy
+    # # dataframe[[ANCHOR]] = dataframe[[ANCHOR]].fillna(value=0)#.astype(int).astype(str)
+    # years = [x if x else ROOT for x in dataframe.Year.unique()]
+    # tree = {y: [] for y in years}
+    # temp = {y: {h: [] for h in hierarchy} for y in years}
+    # i = 0
+    # for index, row in dataframe.iterrows():
+    #     i += 1
+    #     for prev,cur in zip(hierarchy, hierarchy[1:]):
+    #         print(prev,cur)
+    #         if row[cur] is not None:
+    #             continue
+    #         else:
+    #             k = ROOT if row[ANCHOR] is None else row[ANCHOR]
+    #             temp[k][prev].append(build_data_block(row, prev))
+    #             break
+    #
+    #     if i == 13:
+    #         pp.pprint(temp['2013'])
+    #         pdb.set_trace()
+    #         print('halo')
+    #
+    # pdb.set_trace()
+    # print('ha')
 
 
 
