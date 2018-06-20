b200d7b8fa855e057624283a19179a817cd2fe20
Phyo Thiha <phyo.thiha@groupm.com>
2018-06-20 10:24:43 -0400

    Streamlined the args to cache the data as local file. Added a way to save/load from JSON

--- product_mapping/heuristic.py
+++ product_mapping/heuristic.py
@@ -41,6 +41,20 @@ def write_data_to_pickle(data, file):
 def load_data_from_pickle(file):
     with open(file, 'rb') as fi:
         data = pickle.load(fi)
+    print('Data loaded from file:', file)
+    return data
+
+
+def write_data_to_json(json_data, file):
+    with open(file, 'w') as fo:
+        json.dump(json_data, fo)
+    print("JSON data written to:", file)
+
+
+def load_data_from_json(file):
+    with open(file, 'r') as fi:
+        data = json.load(fi)
+    print('Data loaded from file:', file)
     return data
 
 
@@ -102,14 +116,13 @@ def get_suggestions(word_cnt_t1, words_in):
                 subcat_cnt[k] += v
     return subcat_cnt
 
+
 if __name__ == '__main__':
-    MAPPING_FILE = 'mapping_table.p'
-    WORD_COUNT_FILE = 'word_count_table.p'
     OUTPUT_DIR = 'pickled_data'
+    WORD_COUNT_FILE = 'word_count_table.json'
 
     cur_dir_path = os.path.dirname(os.path.realpath(__file__))
     output_dir = os.path.join(cur_dir_path, OUTPUT_DIR)
-    mapping_file = os.path.join(output_dir, MAPPING_FILE)
     word_count_file = os.path.join(output_dir, WORD_COUNT_FILE)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -123,62 +136,40 @@ if __name__ == '__main__':
     For more detail, please use '-h' flag to read the help/manual.
     '''
     parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument('-sm',
+    parser.add_argument('-l',
                         default=0,
                         type=int,
-                        help="If '1', this will generate a serialized file, named '"
-                             + MAPPING_FILE
-                             + "' of existing mapping data in a local directory named, '"
-                             + OUTPUT_DIR + ".'")
-    parser.add_argument('-lm',
-                        default=0,
-                        type=int,
-                        help="If '1', this will try to load mappings from a file named '"
-                             + MAPPING_FILE
-                             + "'in a directory called '" + OUTPUT_DIR
-                             + "'. Else, it'll load directly from the remote database table.")
-    parser.add_argument('-st',
-                        default=0,
-                        type=int,
-                        help="If '1', this will generate a serialized file, named '"
+                        help="If '1', this will USE the local cache file of the word count table named, '"
                              + WORD_COUNT_FILE
-                             + "' of word count table in a local directory named, '"
+                             + "', which is expected to be placed in a local directory named, '"
                              + OUTPUT_DIR + ".'")
 
     args = parser.parse_args()
-    if args.sm:
-        write_data_to_pickle(get_data_from_query('all_mappings'), mapping_file)
+    if args.l:
+        word_cnt_tbl = load_data_from_json(word_count_file) #json.loads(load_data_from_pickle(word_count_file))
     else:
-        if args.lm:
-            print("Loading data from file:", mapping_file)
-            data = json.loads(load_data_from_pickle(mapping_file))
-        else:
-            query_name = 'all_mappings'
-            print("Loading data from remote database using query:", QUERIES[query_name])
-            data = json.loads(get_data_from_query(query_name))
+        query_name = 'all_mappings'
+        print('Loading data from remote database using query:', QUERIES[query_name])
+        mapping_data = json.loads(get_data_from_query(query_name))
 
         SIGNALS = ['GM_ADVERTISER_NAME', 'GM_SECTOR_NAME', 'GM_SUBSECTOR_NAME',
                    'GM_CATEGORY_NAME', 'GM_BRAND_NAME', 'GM_PRODUCT_NAME']
-        word_cnt_t1 = build_total_word_cnt_table(data, SIGNALS)
-        # word_cnt_t2 = build_column_specific_table(data, SIGNALS)
-        if args.st:
-            write_data_to_pickle(word_cnt_t1, word_count_file)
+        word_cnt_tbl = build_total_word_cnt_table(mapping_data, SIGNALS)
+        # word_cnt_tbl = build_column_specific_table(mapping_data, SIGNALS)
+        write_data_to_json(word_cnt_tbl, word_count_file)
 
-        while True:
-            words_in = input('Enter your input (or enter to exit): ')
-            if len(words_in) == 0:
-                print
-                "goodbye"
-                break
-            print
-            suggestions = get_suggestions(word_cnt_t1, words_in)
-            print(suggestions)
-            '-----'
-            for w in suggestions:
-                print(w)
-            '-----'
-            print
-            " "
-        print("Data loaded")
-        # 'CP_SUBCATEGORY_NAME' => 'CP_SUBCATEGORY_ID'
-        # 'CP_VARIANT_NAME' => 'CP_VARIANT_ID_1PH'
+    print("Data loaded and ready to start predicting.\n")
+    while True:
+        words_in = input('Enter your input (or enter to exit): ')
+        if len(words_in) == 0:
+            print('Program finished.')
+            break
+
+        suggestions = get_suggestions(word_cnt_tbl, words_in)
+        print(suggestions)
+        print("<-----Suggestions sorted by frequency-----")
+        for k in sorted(suggestions, key=suggestions.get, reverse=True):
+            print(k,"",suggestions[k])
+        print("----->\n")
+    # 'CP_SUBCATEGORY_NAME' => 'CP_SUBCATEGORY_ID'
+    # 'CP_VARIANT_NAME' => 'CP_VARIANT_ID_1PH'
