121ec7f6436cdb50f1b05e0a88225c050fe93cd7
Phyo Thiha <lacheephyo@gmail.com>
2018-07-20 15:26:08 -0400

    Started tuning and testing parameters (like an idiot). Will try hyper parameter tuning later with Tensorflow

--- product_mapping/keras_nn.py
+++ product_mapping/keras_nn.py
@@ -1,14 +1,16 @@
 import pdb
+
+import time
+
 import numpy
 import pandas
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout, Activation
 from keras.models import model_from_json
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras.utils import np_utils
-from keras.optimizers import SGD
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
+from keras.optimizers import SGD, RMSprop, Adagrad, Adadelta, Adam, Adamax, Nadam, TFOptimizer
+from sklearn.model_selection import cross_val_score, train_test_split, KFold
 from sklearn.preprocessing import LabelEncoder
 
 
@@ -35,6 +37,7 @@ from sklearn.preprocessing import LabelEncoder
 # fix random seed for reproducibility
 seed = 7
 numpy.random.seed(seed)
+stamp = str(int(time.time()))
 
 # load dataset
 dataframe = pandas.read_csv("./input/raw_mappings.csv", header=None) # numpy.loadtxt('./input/raw_mappings.csv', delimiter=',')
@@ -51,26 +54,66 @@ encoded_Y = encoder.transform(Y)
 # convert integers to dummy variables (i.e. one hot encoded)
 dummy_y = np_utils.to_categorical(encoded_Y)
 
-pdb.set_trace()
+# split into 80-20 for training and testing
+X_train, X_test, Y_train, Y_test = train_test_split(X, dummy_y, test_size=0.2, random_state=seed)
+
 # create model
 model = Sequential()
-model.add(Dense(100, input_dim=X_len, activation='relu')) # TODO: change NN layer config
-model.add(Dense(Y_cnt, activation='softmax'))
-# Compile model
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy',
-              # optimizer='adam', # 35% accuracy
-                optimizer=sgd,
-              metrics=['accuracy'])
-model.fit(X, dummy_y, epochs=100, batch_size=10000, verbose=1) # 35% accuracy one
-score = model.evaluate(x_test, y_test, batch_size=128)
-
+activation_func = 'softplus'
+model.add(Dense(X_len+X_len,#int(X_len/2),
+                input_dim=X_len, activation=activation_func)) # fully-connected layer with X_len/2 hidden units
+model.add(Dropout(0.5))
+#softmax; elu; selu; softplus; softsign; relu; tanh; sigmoid; hard_sigmoid; linear
+model.add(Dense(X_len+X_len, activation=activation_func)) # softmax/softplus(38%); elu/selu/relu(38%);
+model.add(Dropout(0.5))
+model.add(Dense(X_len+X_len, activation=activation_func))
+model.add(Dropout(0.5))
+# softplus-softplus-40p; softmax-softmax-38p; softsign-softsign-02%;
+# tanh-tanh-02%; sigmoid-sigmoid-38p; hard_sigmoid-hard_sigmoid-0.2p; linear-linear-5%;
+model.add(Dense(Y_cnt, activation=activation_func))
 # serialize model to JSON
 model_json = model.to_json()
-with open("model.json", "w") as json_file:
+with open("model_"+stamp+".json", "w") as json_file:
     json_file.write(model_json)
+
+## Compile model
+# Optimizers: https://keras.io/optimizers/
+#sgd; RMSprop; Adagrad; Adadelta Adam; Adamax; Nadam
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+rmsprop = RMSprop(lr=0.001, rho=0.9, epsilon=None, decay=0.0)
+adagrad = Adagrad(lr=0.01, epsilon=None, decay=0.0)
+adadelta = Adadelta(lr=1.0, rho=0.95, epsilon=None, decay=0.0)
+adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+adamax = Adamax(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0)
+nadam = Nadam(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=None, schedule_decay=0.004)
+# TODO: learn how to use TFOptimizer(optimizer)
+model.compile(loss='categorical_crossentropy',
+              # optimizer='adam', # 35% accuracy
+                optimizer=adamax,
+              # [sgd is Meh] sgd 46% at epoch 100; with Xlen+Xlen, Xlen+Xlen, Xlen+Xlen, softplus gets 60% at 100ep
+              # [rmsprop is BAD; it forgets easily and quickly] rmsprop 48% only at epoch 19 and after is bad;
+              # [adagrad does NOT perform in wide networks] adagrad 48% at epoch 100; stalled at just 38% for (Xlen+Xlen)*3 config
+              # [NEVER USE THIS/adadelta] adadelta learns faster and reaches 46% at just epoch 39, but dropps off afterward; if we use X_len as second hidden layer and use adadelta, it peaks at 47% at epoch 34
+              # ** adam goes up to 48% at epoch 100; adamax goes up to 49% at epoch 100, has a lot of potential for longer epoch
+              # [adam is just okay] adam with 3*(Xlen+Xlen) peaked at 60% and is slow
+              # [nadam is BAD; never goes up from 38% for wide nets like 3*(Xlen+Xlen)] nadam tops at 46% after 100 epoch
+
+                # adamax with X_len, 100, and softplus gets 54% at epoch 100
+              # adamax with X_len , X_len, softplus gets 56% at 100 ep
+              # adamax with X_len, X_len, X_len, softplus gets 55% at 100 ep
+              # adamax with Xlen, Xlen+Xlen, Xlen, softplus gets 58% at 100ep and has potential to go a bit higher
+              # adamax with Xlen+Xlen, Xlen+Xlen, Xlen, softplus gets 65% at 100ep
+              # adamax with Xlen+Xlen, Xlen+Xlen, Xlen+Xlen, softplus gets 66% at 100ep
+
+              metrics=['accuracy'])
+# REF: https://machinelearningmastery.com/evaluate-performance-deep-learning-models-keras/
+# https://keras.io/getting-started/sequential-model-guide/
+batch_size = 100
+model.fit(X_train, Y_train, epochs=100, batch_size=batch_size, verbose=1)
+score = model.evaluate(X_test,Y_test, batch_size=batch_size)
+print(score)
 # serialize weights to HDF5
-model.save_weights("model.h5")
+model.save_weights("model_"+stamp+".h5")
 print("Saved model to disk")
 
 ## to use this later...
@@ -90,6 +133,9 @@ print("Saved model to disk")
 # print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1] * 100))
 
 # TODO:
+# Look at example below and see if I can replicate similar Tensorflow in Keras:
+# https://github.com/keras-team/keras/issues/7818
+# Convolution layer: https://keras.io/layers/convolutional/
 # Try this: Multilayer Perceptron (MLP) for multi-class softmax classification
 # https://keras.io/getting-started/sequential-model-guide/
 # http://www.riptutorial.com/tensorflow/example/30750/math-behind-1d-convolution-with-advanced-examples-in-tf
