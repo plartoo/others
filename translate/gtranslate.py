94c58189fbf42608f9c4eb9ade3884cb7e943fcb
Phyo Thiha <lacheephyo@gmail.com>
2018-07-25 15:23:59 -0400

    Removed notes and some old lines of code

--- translate/gtranslate.py
+++ translate/gtranslate.py
@@ -44,81 +44,42 @@ def translate(text, source_language, target_language):
     response = conn.getresponse().read()
     return json.loads(response)
 
-    # # Using Microsoft Bing's Translate API (my poor man's hack-ish attempt)
-    # base_url_1 = 'https://api.microsofttranslator.com/v2/ajax.svc/TranslateArray?appId='
-    # app_id = quote('"') + 'TIzUkShc-PwdtTGuWT6dBOmttItmhca2MXmdccZow2N4*' + quote('"')
-    # base_url_2 = '&texts=['
-    # text = quote('"') + quote(text) + quote('"')
-    # base_url_3 = ']&from='
-    # source_lang = quote('"') + source_language + quote('"')
-    # base_url_4 = '&to='
-    # target_lang = quote('"') + target_language + quote('"')
-    # base_url_5 = '&oncomplete=_mstc5&onerror=_mste5'
-    # url = base_url_1 + app_id + base_url_2 + text + base_url_3 + source_lang + base_url_4 + target_lang + base_url_5
 
-    # # Google's Translation API (no longer works)
-    # base_url= 'https://translate.googleapis.com/translate_a/single?client=gtx&dt=t'
-    # source_lang= '&sl=' + source_language
-    # target_lang= '&tl=' + target_language
-    # text= '&q=' + text
-    # url = base_url + source_lang + target_lang + text
+def clean_text(text):
+    str_before_comma=text.split(',')[0]
+    regex = re.compile('[^a-zA-Z]') # remove non-alphabetical stuff
+    str_with_only_alphabets = regex.sub(' ', str_before_comma)
+    str_with_just_one_space_allowed = ' '.join(str_with_only_alphabets.split())
+    str_lowered = str_with_just_one_space_allowed.lower() # this has positive effect on translation accuracy
+    return str_lowered
 
-    # rq = requests.get(url)
-    # if rq.status_code == 200:
-    #     ## Use following with Microsoft API
-    #     regex = re.search(r'.*\((\[\{.*\}\])\)', rq.text)
-    #     if regex:
-    #         translated = json.load(io.StringIO(regex[1]))
-    #         return translated[0]['TranslatedText']
-    #     else:
-    #         return rq.text
-    #
-    #     ## Use following with Google API
-    #     # translated_text = json.load(io.StringIO(rq.text))
-    #     # return translated_text[0][0][0]
-    # return error_msg
 
-print(
-    translate('Ya hemos mirado la configuración en el panel de control como Miguel nos explicó pero el  problema  sigue existiendo',
-      'es',
-      'en')
-)
+if __name__ == "__main__":
+    source_lang = 'de'
+    target_lang = 'en'
+    df = pd.read_excel(open('to_translate.xlsx', 'rb'), sheetname='Products_Mapping')
+    cols = ['GM_COUNTRY_NAME', 'GM_ADVERTISER_NAME', 'GM_SECTOR_NAME', 'GM_CATEGORY_NAME', 'GM_BRAND_NAME',
+            'GM_PRODUCT_NAME', 'SOS_PRODUCT']
+    cols_to_translate = ['GM_COUNTRY_NAME', 'GM_ADVERTISER_NAME', 'GM_SECTOR_NAME', 'GM_CATEGORY_NAME', 'GM_BRAND_NAME',
+                         'GM_PRODUCT_NAME']
+    translated_output = []
 
-# def clean_text(text):
-#     str_before_comma=text.split(',')[0]
-#     regex = re.compile('[^a-zA-Z]') # remove non-alphabetical stuff
-#     str_with_only_alphabets = regex.sub(' ', str_before_comma)
-#     str_with_just_one_space_allowed = ' '.join(str_with_only_alphabets.split())
-#     str_lowered = str_with_just_one_space_allowed.lower() # this has positive effect on translation accuracy
-#     return str_lowered
-#
-#
-# if __name__ == "__main__":
-#     source_lang = 'de'
-#     target_lang = 'en'
-#     df = pd.read_excel(open('to_translate.xlsx', 'rb'), sheetname='Products_Mapping')
-#     cols = ['GM_COUNTRY_NAME', 'GM_ADVERTISER_NAME', 'GM_SECTOR_NAME', 'GM_CATEGORY_NAME', 'GM_BRAND_NAME',
-#             'GM_PRODUCT_NAME', 'SOS_PRODUCT']
-#     cols_to_translate = ['GM_COUNTRY_NAME', 'GM_ADVERTISER_NAME', 'GM_SECTOR_NAME', 'GM_CATEGORY_NAME', 'GM_BRAND_NAME',
-#                          'GM_PRODUCT_NAME']
-#     translated_output = []
-#
-#     for i, r in df.iterrows():
-#         cur_row = []
-#         for c in cols_to_translate:
-#             cur_row.append(r[c])
-#             cleaned_txt = clean_text(r[c])
-#             translated_text = translate(cleaned_txt, source_lang, target_lang)
-#             cur_row.append(translated_text)
-#         cur_row.append(r['SOS_PRODUCT'])
-#         print(cur_row)
-#         translated_output.append(cur_row)
-#
-#     final_cols = [j for c in cols_to_translate for j in [c, '{}_Translated'.format(c)]] + ['SOS_PRODUCT']
-#     df_out = pd.DataFrame(translated_output, columns=final_cols)
-#     df_out.to_excel('translated.xlsx', index=False)
-#     # writer = pd.ExcelWriter('translated.xlsx')
-#     # df_out.to_excel(writer, index=False)
-#     # writer.save()
-#     print('Translation finished')
+    for i, r in df.iterrows():
+        cur_row = []
+        for c in cols_to_translate:
+            cur_row.append(r[c])
+            cleaned_txt = clean_text(r[c])
+            translated_text = translate(cleaned_txt, source_lang, target_lang)
+            cur_row.append(translated_text)
+        cur_row.append(r['SOS_PRODUCT'])
+        print(cur_row)
+        translated_output.append(cur_row)
+
+    final_cols = [j for c in cols_to_translate for j in [c, '{}_Translated'.format(c)]] + ['SOS_PRODUCT']
+    df_out = pd.DataFrame(translated_output, columns=final_cols)
+    df_out.to_excel('translated.xlsx', index=False)
+    # writer = pd.ExcelWriter('translated.xlsx')
+    # df_out.to_excel(writer, index=False)
+    # writer.save()
+    print('Translation finished')
 
