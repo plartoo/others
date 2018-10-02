04e29e16acebd196a8d976734b7ee439e249e264
Phyo Thiha <lacheephyo@gmail.com>
2018-10-02 15:24:03 -0400

    Prepping to start writing the config loader

--- data_architect_misc/colpal_data_cleanser/data_cleanser_utils.py
+++ data_architect_misc/colpal_data_cleanser/data_cleanser_utils.py
@@ -2,4 +2,17 @@ import pdb
 
 import account_info
 
-def load_config()
+# https://web.archive.org/web/20181002170353/https://hackernoon.com/4-ways-to-manage-the-configuration-in-python-4623049e841b
+def load_config():
+    pass
+
+
+def print_config():
+    pass
+
+
+
+{
+
+
+}
