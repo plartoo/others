7d6b92bb880eed5aea0d2f0f2e24f508d2b42c5d
plartoo <lacheephyo@gmail.com>
2019-06-17 23:26:58 -0400

    Playing with Pillow and gitpython packages

---
+++ random_stuff/crop_image/crop_image.py
@@ -0,0 +1,60 @@
+import datetime
+import glob
+import os
+from pathlib import Path
+
+import git # pip install gitpython
+from PIL import Image #pip install Pillow
+
+
+def crop(input_img, coords, output_img):
+    """
+    @param input_img: Path and name of the image to crop
+    @param coords: A tuple of x/y coordinates (x1, y1, x2, y2)
+    @param saved_location: Path and name of the cropped image to save
+    """
+    image_obj = Image.open(input_img)
+    cropped_image = image_obj.crop(coords)
+    cropped_image.save(output_img)
+
+
+if __name__ == '__main__':
+    home = str(Path.home())
+    img_dir = os.path.join(home, 'Dropbox', 'Apps', 'Test')
+    repo_dir = os.path.join(img_dir, 'tootp.github.io')
+
+    # yyyymmdd = datetime.datetime.today().strftime('%Y%m%d')
+    # img_file_name = ''.join([yyyymmdd, '.jpg'])
+    img_files = glob.glob(os.path.join(img_dir, '*.jpg'))
+    img_files.sort(key=os.path.getmtime)
+    last_modified_img = img_files[-1]
+    img_modified_date = datetime.datetime.fromtimestamp(os.path.getmtime(last_modified_img)).date()
+    today_date = datetime.datetime.today().date()
+
+    # REFs: https://stackoverflow.com/q/1456269
+    # https://gitpython.readthedocs.io/en/stable/tutorial.html#handling-remotes
+    repo = git.Repo(repo_dir)
+    repo.remotes.origin.pull() # first, try to sync the repo with remote
+
+    if img_modified_date == today_date:
+        for img_coord in [('a.png', (100, 100, 101, 379)),
+                          ('b.png', (543, 115, 831, 365)),
+                          ('c.png', (22, 630, 1310, 669))]:
+            #[('a.png', (100, 88, 101, 343)),
+            #                  ('b.png', (446, 361, 735, 650)),
+            #                  ('c.png', (10, 1207, 741, 1248))]:
+            output_img = os.path.join(repo_dir , img_coord[0])
+            print("Starting to crop the image at:", last_modified_img)
+            crop(last_modified_img, img_coord[1], output_img)
+            print("Wrote cropped image at:", output_img)
+            repo.git.add(img_coord[0])
+            print("This image file is added to git repo:", img_coord[0])
+
+        repo.git.commit(m="Test commit")
+        print("Committed recently added image files.")
+        repo.remotes.origin.push()
+        print("Pushed committed image files to remote repo.")
+        print(repo.git.status())
+        print("Image cropped and committed to the Github repo.\n")
+    else:
+        print("No image with modified date set to TODAY found.\n")
