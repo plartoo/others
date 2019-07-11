daee4ac789f52a0de1985081289eded7dcf78b5b
Phyo Thiha <lacheephyo@gmail.com>
2019-07-11 15:22:29 -0400

    Moved some creds to account_info after updating the keys in Azure

--- data_architect_misc/data_transformer/azureBlobStorageFile.py
+++ data_architect_misc/data_transformer/azureBlobStorageFile.py
@@ -1,14 +1,15 @@
-
 import os
 from azure.storage.blob import BlobService, BlockBlobService, PublicAccess
 import time
 import urllib.request
 import ssl
 
+import account_info
 
 def copy_azure_files(self):
     # https://stackoverflow.com/questions/32500935/python-how-to-move-or-copy-azure-blob-from-one-container-to-another
-    blob_service = BlockBlobService(account_name='wmdatarfcolgate',account_key='ww2KsXz/Jz29XztHWshmgg1o+Cd8PffTzNcKAumCjiO/VG7j2EhFGk5iezEkpizvY1NHgCTp06CHtcDcd1bh3A==')
+    blob_service = BlockBlobService(account_name=account_info.AZURE_BLOB_ACCNT_NAME,
+                                    account_key=account_info.AZURE_BLOB_ACCNT_KEY)
 
     blob_name = 'test_1534532726.json'
     copy_from_container = 'colgate-palmolive'
@@ -32,8 +33,8 @@ if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
     ssl._create_default_https_context = ssl._create_unverified_context
 
  # Create the BlockBlobService that is used to call the Blob service for the storage account
-block_blob_service = BlockBlobService(account_name='wmdatarfcolgate',
-                                      account_key='ww2KsXz/Jz29XztHWshmgg1o+Cd8PffTzNcKAumCjiO/VG7j2EhFGk5iezEkpizvY1NHgCTp06CHtcDcd1bh3A==')
+block_blob_service = BlockBlobService(account_name=account_info.AZURE_BLOB_ACCNT_NAME,
+                                      account_key=account_info.AZURE_BLOB_ACCNT_KEY)
 
 print("\nList blobs in the container")
 generator = block_blob_service.list_blobs('colgate-palmolive')
@@ -44,7 +45,7 @@ copy_azure_files()
 
 
 
-# url='http://data.groupm.com/api/SavedQuery/a9c450ab-9bb1-4368-9035-c746cba52363?applicationtype=DataMarketplace_API&useremail=phyo.thiha%40groupm.com&applicationtoken=ea55447a-bd17-404c-8539-87ecba7e72d1'
+# url= # see account_info file
 # req = urllib.request.Request(url)
 # result=urllib.request.urlopen(req)
 # #result_bytes = result.read()
