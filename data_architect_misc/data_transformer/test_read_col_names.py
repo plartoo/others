d880190dea31feee74a0204d05a3c3fdc9dbcd7c
Phyo Thiha <lacheephyo@gmail.com>
2019-08-22 16:06:35 -0400

    Renamed 'test.py' to be more specific for columna name testing, 'test_read_col_names.py', and now will start using it to test read_data

--- data_architect_misc/data_transformer/test_read_col_names.py
+++ data_architect_misc/data_transformer/test_read_col_names.py
@@ -5,36 +5,22 @@ import transform_utils
 if __name__ == '__main__':
     config = {
         'row_index_to_extract_column_names': 0,
-        #'custom_column_names_to_assign': ['a','b'],
-        'list_of_column_indexes_to_use':['int_id_1_1_1',
-                                         'date_2019-01-01_2019-12-31_2',
-                                         'ascii_str_8_8_3',
-                                         'integer_-1000_1000_5'
-                                         ],
-        'old_column_name_to_new_column_name_mappings': {
-            'int_id_1_1_1': 'col A',
-            'date_2019-01-01_2019-12-31_2': 'col B',
-            'ascii_str_8_8_3': 'col C',
-            'integer_-1000_1000_5': 'col D'
-        },
-        'num_of_rows_to_skip_from_the_bottom': 5,
-        'max_rows_to_process_each_iteration': 50,
         'input_csv_file_delimiter': '|',
+        #'custom_column_names_to_assign': ['a','b'],
     }
-
     print(transform_utils.get_raw_column_names('csv_n.csv', config))
 
     config = {
         'row_index_to_extract_column_names': 4,
-        #'custom_column_names_to_assign': ['a','b'],
         'input_csv_file_delimiter': '|',
+        #'custom_column_names_to_assign': ['a','b'],
     }
     print(transform_utils.get_raw_column_names('csv_d.csv', config))
 
     config = {
         'row_index_to_extract_column_names': 4,
-        'custom_column_names_to_assign': ['a','b'],
         'input_csv_file_delimiter': '|',
+        'custom_column_names_to_assign': ['a','b'],
     }
     print(transform_utils.get_raw_column_names('csv_d.csv', config))
 
@@ -52,8 +38,8 @@ if __name__ == '__main__':
 
     config = {
         'row_index_to_extract_column_names': 4,
-        'custom_column_names_to_assign': ['a','b'],
         'input_csv_file_delimiter': '|',
+        'custom_column_names_to_assign': ['a','b'],
     }
     print(transform_utils.get_raw_column_names('excel_d.xlsx', config))
 
