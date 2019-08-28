025efaa1b5a03e35a672d3356bb32cfc1d3d608f
Phyo Thiha <lacheephyo@gmail.com>
2019-08-28 16:08:14 -0400

    Just realized that we are not getting iterator back from pandas read_excel. So in order to consolidate read_csv and read_excel into read_data, it either might be too dirty of a hack or it requires us to manually read the chunks. That led me to explore some efficient line counting approaches in Python. Still need more work when I have some downtime later.

--- data_architect_misc/data_transformer/test.py
+++ data_architect_misc/data_transformer/test.py
@@ -79,13 +79,65 @@ if __name__ == '__main__':
     # print("\n====\n")
 
 
+    # ## GOOD TO GO for csv
+    # # how to read WIHTOUT including header row
+    # # header is on rw num #5, data starts at row num #9
+    # for chunk in pd.read_csv(
+    #     'csvv.csv',
+    #     skiprows=8,# row_index_where_data_starts,
+    #     chunksize=chunksize,
+    #     #skipfooter=0,
+    #     header=None,# if there is no header, we have to set this to None
+    #     names=custom_header_names,
+    #     usecols=column_names_or_indexes_to_use,
+    #     dtype={},
+    #     delimiter='|',
+    #     encoding='utf-8'):
+    #     if chunk.shape[0] < chunksize:
+    #         chunk.drop(chunk.tail(2).index, inplace=True)
+    #     print(chunk)
+    #     print(chunk.shape[0])
+    # print("\n====\n")
+    #
+    # # WITHOUT assuming header row
+    # for chunk in pd.read_csv(
+    #         'csv_no_header.csv',
+    #         skiprows=0,# row_index_where_data_starts,
+    #         chunksize=chunksize,
+    #         #skipfooter=0,
+    #         header=None,# if there is no header, we have to set this to None
+    #         names=custom_header_names,
+    #         usecols=column_names_or_indexes_to_use,
+    #         dtype={},
+    #         delimiter='|',
+    #         encoding='utf-8'):
+    #     print(chunk)
+    #     print(chunk.shape[0])
+    # print("\n====\n")
+    #
+    # # WITHOUT assuming header row
+    # for chunk in pd.read_csv(
+    #     'csv.csv',
+    #     skiprows=1, # row_index_where_data_starts, # so we cannot default this to '1'; we must default this to '0'
+    #     chunksize=chunksize,
+    #     #skipfooter=0,
+    #     header=None,
+    #     names=custom_header_names,
+    #     usecols=column_names_or_indexes_to_use,
+    #     dtype={},
+    #     delimiter='|',
+    #     encoding='utf-8'):
+    #     print(chunk)
+    #     print(chunk.shape[0])
+    #
+    # print("\n====\n")
+
     # how to read WIHTOUT including header row
     # header is on rw num #5, data starts at row num #9
-    for chunk in pd.read_csv(
-        'csvv.csv',
+    for chunk in pd.read_excel(
+        'excell.xlsx',
         skiprows=8,# row_index_where_data_starts,
-        chunksize=chunksize,
-        skipfooter=0,
+        nrows=chunksize,
         header=None,# if there is no header, we have to set this to None
         names=custom_header_names,
         usecols=column_names_or_indexes_to_use,
@@ -98,35 +150,38 @@ if __name__ == '__main__':
         print(chunk.shape[0])
     print("\n====\n")
 
-    # WITHOUT assuming header row
-    for chunk in pd.read_csv(
-            'csv_no_header.csv',
-            skiprows=0,# row_index_where_data_starts,
-            chunksize=chunksize,
-            skipfooter=0,
-            header=None,# if there is no header, we have to set this to None
-            names=custom_header_names,
-            usecols=column_names_or_indexes_to_use,
-            dtype={},
-            delimiter='|',
-            encoding='utf-8'):
-        print(chunk)
-        print(chunk.shape[0])
-    print("\n====\n")
-
-    # WITHOUT assuming header row
-    for chunk in pd.read_csv(
-        'csv.csv',
-        skiprows=1, # row_index_where_data_starts, # so we cannot default this to '1'; we must default this to '0'
-        chunksize=chunksize,
-        skipfooter=0,
-        header=None,
-        names=custom_header_names,
-        usecols=column_names_or_indexes_to_use,
-        dtype={},
-        delimiter='|',
-        encoding='utf-8'):
-        print(chunk)
-        print(chunk.shape[0])
+    # # WITHOUT assuming header row
+    # for chunk in pd.read_excel(
+    #         'excel_no_header.xlsx',
+    #         skiprows=0,# row_index_where_data_starts,
+    #         chunksize=chunksize,
+    #         header=None,# if there is no header, we have to set this to None
+    #         names=custom_header_names,
+    #         usecols=column_names_or_indexes_to_use,
+    #         dtype={},
+    #         delimiter='|',
+    #         encoding='utf-8'):
+    #     print(chunk)
+    #     print(chunk.shape[0])
+    # print("\n====\n")
+    #
+    # # WITHOUT assuming header row
+    # for chunk in pd.read_excel(
+    #     'excel.xlsx',
+    #     skiprows=1, # row_index_where_data_starts, # so we cannot default this to '1'; we must default this to '0'
+    #     chunksize=chunksize,
+    #     header=None,
+    #     names=custom_header_names,
+    #     usecols=column_names_or_indexes_to_use,
+    #     dtype={},
+    #     delimiter='|',
+    #     encoding='utf-8'):
+    #     print(chunk)
+    #     print(chunk.shape[0])
 
     print("\n====\n")
+
+# Possible Approaches
+# 1. convert XLSX to CSV and then consolidate read_data into that
+# 2. read ALL (max possible) lines in XLSX
+# 3. Find out row number in Excel file and use nrows to read chunk by chunk
