312cebb995298b9b48156b3f53d9a9ab5c5787e7
plartoo <lacheephyo@gmail.com>
2019-08-30 22:26:06 -0400

    Added comments and reorganized some code during half-day work

--- data_architect_misc/data_extractors/rentrak/rentrak_selenium.py
+++ data_architect_misc/data_extractors/rentrak/rentrak_selenium.py
@@ -1,7 +1,24 @@
+import pdb
+
+"""
+Author: Phyo Thiha
+Last Modified Date: August 30, 2019
+Description: This is a Selenium script used to download data from RenTrak's website. 
+For anyone interested, read the following resources to learn more about Selenium:
+# Selenium Docs: http://selenium-python.readthedocs.io/
+# http://web.archive.org/web/20190830175223/http://thiagomarzagao.com/2013/11/12/webscraping-with-selenium-part-1/
+# http://web.archive.org/web/20190830175349/http://thiagomarzagao.com/2013/11/14/webscraping-with-selenium-part-2/
+# http://web.archive.org/web/20190830175411/http://thiagomarzagao.com/2013/11/15/webscraping-with-selenium-part-3/
+# http://web.archive.org/web/20190830175437/https://www.scrapehero.com/tutorial-web-scraping-hotel-prices-using-selenium-and-python/
+"""
+
 import os
 
 from selenium import webdriver
 
+
+BASE_URL = 'https://national-tv.rentrak.com'
+# assumes that chromedriver exe file is in the same root folder as this script
 CHROME_DRIVER_FOLDER_NAME = 'chromedriver'
 path_to_chromedriver = os.path.join(os.path.dirname(os.path.normpath(os.getcwd())), CHROME_DRIVER_FOLDER_NAME)
 
