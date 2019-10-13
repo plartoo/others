686cdd07112ac327dcbb7d39e8d7aa4eef3b3ac6
plartoo <lacheephyo@gmail.com>
2019-10-13 12:44:06 -0400

    Finished script to calculate L+3 ratings weighted by market-based index

--- data_architect_misc/data_extractors/rentrak/calculate_colpal_ratings.py
+++ data_architect_misc/data_extractors/rentrak/calculate_colpal_ratings.py
@@ -19,6 +19,7 @@ ONLY AFTER THAT, run this script.
 import pdb
 
 import argparse
+from collections import defaultdict
 from datetime import datetime
 import re
 import os
@@ -49,7 +50,6 @@ df_l3 = pd.read_excel(l3_rating_file, sheet_name=l3_rating_sheets,
                                'Net', 'Day', 'National Time', # market name, market-weighted telecast ratings
                                'L+3 Avg Tlcst Rtg', 'Avg Tlcst Aud'],
                       skiprows=6, skipfooter=7)
-
 df_index = pd.read_csv(index_file)
 
 min_date = min([df_l3[k]['Day'].min() for k in df_l3.keys()]).strftime('%Y-%m')
@@ -57,36 +57,30 @@ max_date = max([df_l3[k]['Day'].max() for k in df_l3.keys()]).strftime('%Y-%m')
 print("Selecting index data between these date ranges (they are usually the same):", min_date, "and", max_date)
 df_index = df_index[(df_index['Date'] >= min_date) & (df_index['Date'] <= max_date)]
 
-final_df = pd.DataFrame(columns=[
-    'Advertiser', 'Brand', 'Product', 'AdCopy',
-    'DurationInSecs', 'NetworkCode', 'Day', 'NationalTime',
-    'L3AverageTelecastRatings', 'AverageTelecastAudience',
-    'MarketName', 'Index', 'MarketWeightedTelecastRatings'])
-
+final_df_dict = defaultdict(list)
 for advertiser in df_l3.keys():
     # We will drop dupes, if any, because I have found in raw data file that there are usually duplicates
     cur_df_l3 = df_l3[advertiser].drop_duplicates()
     for df_l3_index, df_l3_row in cur_df_l3.iterrows():
+        print("\n", df_l3_row)
         network_code = df_l3_row['Net']
         year_month = df_l3_row['Day'].strftime('%Y-%m')
-        relevant_rating_index = df_index[(df_index['NetworkCode'] == network_code) & (df_index['Date'] == year_month)]
-        for rating_index_index, rating_index_row in relevant_rating_index.iterrows():
-            final_df = final_df.append(
-                {'Advertiser': df_l3_row['Advertiser'],
-                 'Brand': df_l3_row['Brand'],
-                 'Product': df_l3_row['Product'],
-                 'AdCopy': df_l3_row['Ad Copy'],
-                 'DurationInSecs': df_l3_row['Duration (secs)'],
-                 'NetworkCode': network_code,
-                 'Day': df_l3_row['Day'],
-                 'NationalTime': df_l3_row['National Time'],
-                 'L3AverageTelecastRatings': df_l3_row['L+3 Avg Tlcst Rtg'],
-                 'AverageTelecastAudience': df_l3_row['Avg Tlcst Aud'],
-                 'MarketName': rating_index_row['MarketName'],
-                 'Index': rating_index_row['Index'],
-                 'MarketWeightedTelecastRatings': rating_index_row['Index'] * df_l3_row['L+3 Avg Tlcst Rtg'],
-                 }, ignore_index=True)
+        relevant_rating_index_df = df_index[(df_index['NetworkCode'] == network_code) & (df_index['Date'] == year_month)]
+        for rating_index_index, rating_index_row in relevant_rating_index_df.iterrows():
+            final_df_dict['Advertiser'].append(df_l3_row['Advertiser'])
+            final_df_dict['Brand'].append(df_l3_row['Brand'])
+            final_df_dict['Product'].append(df_l3_row['Product'])
+            final_df_dict['AdCopy'].append(df_l3_row['Ad Copy'])
+            final_df_dict['DurationInSecs'].append(df_l3_row['Duration (secs)'])
+            final_df_dict['NetworkCode'].append(network_code)
+            final_df_dict['Day'].append(df_l3_row['Day'])
+            final_df_dict['NationalTime'].append(df_l3_row['National Time'])
+            final_df_dict['L3AverageTelecastRatings'].append(df_l3_row['L+3 Avg Tlcst Rtg'])
+            final_df_dict['AverageTelecastAudience'].append(df_l3_row['Avg Tlcst Aud'])
+            final_df_dict['MarketName'].append(rating_index_row['MarketName'])
+            final_df_dict['Index'].append(rating_index_row['Index'])
+            final_df_dict['MarketWeightedTelecastRatings'].append(rating_index_row['Index'] * df_l3_row['L+3 Avg Tlcst Rtg'])
 
-pdb.set_trace()
+df_final = pd.DataFrame.from_dict(final_df_dict)
+df_final.to_csv(output_file)
 print("Colpal weighted ratings written at:", output_file)
-
