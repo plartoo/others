639b5f970c7ec3e8f7df8c6666b09a925753731e
plartoo <lacheephyo@gmail.com>
2019-11-12 22:06:42 -0500

    Updated send sms script as I keep experimenting new ways to do it

--- random_stuff/twilio/send_sms.py
+++ random_stuff/twilio/send_sms.py
@@ -12,6 +12,14 @@ from twilio.rest import Client
 CONFIG_FILE_NAME = 'config.json'
 
 def load_config():
+    # config.json file should be like this:
+    # {
+    #     "acnt_sid": "",
+    #     "auth_token": "",
+    #     "messaging_service_sid": "",
+    #     "from_num": "+12018000000",
+    #     "test_to_num": "+17188000000"
+    # }
     # We can extend this method to allow params, but for now, this is sufficient
     this_dir = os.path.dirname(__file__)
     config_file = open(os.path.join(this_dir, CONFIG_FILE_NAME))
@@ -33,8 +41,9 @@ if __name__ == '__main__':
     message = client.messages.create(
         #from_=config['from_num'],
         messaging_service_sid=config['messaging_service_sid'],
-        to=config['test_to_num'],
+        to='2018694715',#config['test_to_num'],
         body=message_body,
     )
+    pdb.set_trace()
     print(message.sid)
     
