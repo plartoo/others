2c8aee8d52e02fefde6154379c101b9d2d50e873
plartoo <lacheephyo@gmail.com>
2019-11-23 21:57:20 -0500

    Updated send sms script; created send_gmail script and its util; wrote steps down in a text file so that I can remind myself

---
+++ random_stuff/twilio/send_gmail.py
@@ -0,0 +1,108 @@
+import pdb
+
+import base64
+import json
+import pickle
+import os.path
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from email.mime.text import MIMEText
+from googleapiclient.errors import HttpError
+import pandas as pd
+
+
+import gmail_utils
+
+
+def get_full_path(dir_name):
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), dir_name)
+
+
+def myprint(j):
+    print(json.dumps(j, sort_keys=True, indent=4))
+
+
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    TOKEN_FILE = 'woodcliffreminder_token.pickle'
+    CREDENTIAL_FILE = 'woodcliffreminder_credentials.json'
+    EMAIL_DATA_FILE = 'residents_with_valid_numbers_20191123.xlsx'
+
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(TOKEN_FILE):
+        with open(TOKEN_FILE, 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CREDENTIAL_FILE, gmail_utils.SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(TOKEN_FILE, 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    token_file_with_path = get_full_path(TOKEN_FILE)
+    cred_file_with_path = get_full_path(CREDENTIAL_FILE)
+    service = gmail_utils.get_service(token_file_with_path, cred_file_with_path)
+    user_id = 'me'
+    # msg_list = gmail_utils.list_message_with_matching_query(service,
+    #                                                         user_id,
+    #                                                         query='from:no-reply@accounts.google.com')
+    # for m in msg_list:
+    #     msg_id = m.get('id')
+    #     full_msg = gmail_utils.get_full_message(service, 'me', msg_id)
+    #     myprint(full_msg)
+    #     print(gmail_utils.get_full_message_body_str(full_msg))
+
+    df = pd.read_excel(EMAIL_DATA_FILE)
+    # df = pd.read_csv(EMAIL_DATA_FILE)
+    subject = 'Woodcliff Gardens Reminder System: Voting day for Board Election on November 26, 2019 (this Tuesday) at 7PM, Henry Hudson room (8700 basement)'
+    message_text = """
+    (Note: Please do not reply to this email because on the other side, it's a computer system sending these messages out.)
+    <br><br>
+    Hello Shareholders,
+    <br><br>
+    Don't forget to vote for Annual Board Election on <b>November 26 (upcoming Tuesday) at 7PM, Henry Hudson room (8700 basement)!</b>
+    <br><br>
+    You can drop off your absentee ballot or proxy into the management office BEFORE Monday November 25th at 4PM.
+    <br><br>
+    <b>***Please remember every VOTE counts (and we would have to spend thousands of dollars AGAIN if the vote quorum is not met)!!***</b>
+    <br><br><br><br>
+    Thank you!
+    <br>
+    """
+
+    sent_emails = {}
+    for index, row in df.iterrows():
+        raw_to_email = row['EmailCombinedValues']
+        if not (pd.isna(raw_to_email) or raw_to_email == 'NA'):
+            emails = raw_to_email.split(';')
+            to_emails = []
+
+            for e in emails:
+                if (e in sent_emails) or (e == ''):
+                    continue
+                else:
+                    sent_emails[e] = 1
+                    to_emails.append(e)
+
+            to_email = ';'.join(to_emails)
+            if to_email:
+                print(index,str(to_email))
+                msg = gmail_utils.create_message('woodcliffreminder@gmail.com', to_email, subject, message_text)
+                gmail_utils.send_message(service, user_id, msg)
+
+
+if __name__ == '__main__':
+    main()
---
+++ random_stuff/twilio/send_sms_reminders.py
@@ -0,0 +1,57 @@
+import pdb
+
+import json
+import os
+import pprint
+import requests
+from urllib.parse import urljoin, urlparse, urlsplit, quote, unquote, urlencode
+
+import pandas as pd
+from twilio.rest import Client
+
+
+CONFIG_FILE_NAME = 'config.json'
+PHONE_DATA_FILE = 'residents_with_valid_numbers_20191123.csv'
+
+def load_config():
+    # config.json file should be like this:
+    # {
+    #     "acnt_sid": "",
+    #     "auth_token": "",
+    #     "messaging_service_sid": "",
+    #     "from_num": "+12018000000",
+    #     "test_to_num": "+17188000000"
+    # }
+    # We can extend this method to allow params, but for now, this is sufficient
+    this_dir = os.path.dirname(__file__)
+    config_file = open(os.path.join(this_dir, CONFIG_FILE_NAME))
+    config = json.load(config_file)
+    config_file.close()
+    return config
+
+
+if __name__ == '__main__':
+    config = load_config()
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(config)
+
+    # message_body = "(cont. from Woodcliff Gardens Event Reminder System) You can ask candidates questions via this online form (please fill out before noon of Nov 12): https://forms.gle/cU8AJCukXWbPsiqy6. We also suggest you to take this pre-election survey as well https://forms.gle/MbiGMTSPgyAv44Sn8 to let the (current and future) board know about your priorities."
+    message_body = "From Woodcliff Gardens Event Reminder System => Don't forget to vote for Annual Board Election on November 26 (upcoming Tuesday) at 7PM, Henry Hudson room (8700 basement)! You can drop off your absentee ballot or proxy into the management office BEFORE Monday November 25th at 4PM."
+    client = Client(config['acnt_sid'], config['auth_token'])
+
+    # df = pd.read_excel(PHONE_DATA_FILE)
+    df = pd.read_csv(PHONE_DATA_FILE)
+    # for to_num in [config['test_to_num'], '540-630-2250', '5406302250']:
+    for index, row in df.iterrows():
+        # pdb.set_trace()
+        to_num = row['CellOrHomePhoneValues']
+        if not (pd.isna(to_num) or to_num == 0):
+            # print(index,str(to_num))
+            message = client.messages.create(
+                # from_=config['from_num'],
+                messaging_service_sid=config['messaging_service_sid'],
+                to=str(to_num),
+                body=message_body,
+            )
+            print(index, '.', to_num, ':', message.sid)
+
