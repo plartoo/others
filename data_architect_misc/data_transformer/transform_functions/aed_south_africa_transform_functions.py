c47869c652c9cdb92d3bf25986f618bb913e3e34
plartoo <plartoo@users.noreply.github.com>
2020-04-14 17:06:37 -0400

    Wrapper custom transform function module for Kazakh and South Africa.

---
+++ data_architect_misc/data_transformer/transform_functions/aed_south_africa_transform_functions.py
@@ -0,0 +1,26 @@
+"""This is the subclass of Transform function for South Africa (AED division).
+
+We will define transform functions specific to South Africa here.
+
+Author: Maicol Contreras
+Last Modified: April 13, 2020
+"""
+
+import pandas as pd
+
+from transform_functions.comp_harm_transform_functions import CommonCompHarmTransformFunctions
+
+
+class CustomFunctions(CommonCompHarmTransformFunctions):
+    """
+    All custom (uncommon) transform functions **SPECIFIC to
+    individual processing task** must be defined as part
+    of this class.
+    """
+
+    def implement_country_specific_transform_functions_in_this_file_like_this(
+            self,
+            df
+    ) -> pd.DataFrame:
+        """Insert description here."""
+        return df
