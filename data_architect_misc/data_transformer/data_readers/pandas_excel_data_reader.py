9fa72e76f282360d86e4df938c9d98783bd95bd1
plartoo <plartoo@users.noreply.github.com>
2020-05-02 23:06:27 -0400

    Deleted some old files and renamed data reader files to make their names more distinctive.

--- data_architect_misc/data_transformer/data_readers/pandas_excel_data_reader.py
+++ data_architect_misc/data_transformer/data_readers/pandas_excel_data_reader.py
@@ -6,25 +6,24 @@ from datetime import datetime
 import logging
 import os
 
-import transform_utils
 
-
-class ExcelDataReader:
+class PandasExcelDataReader:
     """
-    TODO 0: refactor CSV_data_writer and sql_server_data_writer as I did to excel_data_writer.py
-    TODO 1: Write description here
-    TODO 2: create a mapping of key names between transform and Excel Reader class in transform_utils.py
-
+    This class uses Pandas read_excel to read Pandas dataframe
+    from an Excel file. It supports several most commonly-used
+    parameters of read_excel which are defined as class
+    CONSTANTS below.
     """
-    # Default params
+    # Parameters supported from Pandas' read_excel method
     KEY_SHEET_NAME = 'sheet_name' # 'sheet_name_of_input_excel_file'
     DEFAULT_SHEET_TO_READ = 0
 
-    # Optional params
-    KEY_HEADER = 'header' # 'row_index_to_extract_column_headers'
-    DEFAULT_HEADER = 0 # By default, assume first row as header row
     KEY_KEEP_DEFAULT_NA = 'keep_default_na' # 'interpret_na_null_etc_values_from_raw_data_as_nan'
     DEFAULT_KEEP_DEFAULT_NA = False
+
+    KEY_HEADER = 'header' # 'row_index_to_extract_column_headers'
+    DEFAULT_HEADER = 0 # By default, assume first row as header row
+
     KEY_ROWS_PER_READ_ITERATION = 'rows_per_read'
     DEFAULT_ROWS_PER_READ_ITERATION = 500000
 
