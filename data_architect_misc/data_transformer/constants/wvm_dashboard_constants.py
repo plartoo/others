b2e90ef29f2acc5cda7897ae69d1674c797aa0e0
plartoo <plartoo@users.noreply.github.com>
2020-06-02 19:37:23 -0400

    Created new constant files for FX rate processing, and updated comp harm constants to fix a typo and a forgotten country name.

---
+++ data_architect_misc/data_transformer/constants/wvm_dashboard_constants.py
@@ -0,0 +1,73 @@
+from constants.comp_harm_constants import COUNTRIES
+
+FX_COUNTRY_NAME_TO_HARMONIZED_COUNTRY_NAME_MAPPINGS = {
+     'ARGENTINA': 'Argentina'
+    ,'AUSTRALIA': 'Australia'
+    ,'BRAZIL': 'Brazil'
+    ,'CANADA': 'Canada'
+    ,'CHILE': 'Chile'
+    ,'CHINA': 'China'
+    ,'COLOMBIA': 'Colombia'
+    ,'COSTA RICA': 'Costa Rica'
+    ,'CZECH REPUBLIC': 'Czech Republic'
+    ,'DENMARK': 'Denmark'
+    ,'DOM. REP.': 'Dominican Republic'
+    ,'ECUADOR': 'Ecuador'
+    ,'EL SALVADOR': 'El Salvador'
+    ,'ESTONIA': 'Estonia'
+    ,'EURO': 'Euro'
+    ,'GUATEMALA': 'Guatemala'
+    ,'HONDURAS': 'Honduras'
+    ,'HONG KONG': 'Hong Kong'
+    ,'HUNGARY': 'Hungary'
+    ,'INDIA': 'India'
+    ,'INDONESIA': 'Indonesia'
+    ,'ISRAEL': 'Israel'
+    ,'KAZAKHSTAN': 'Kazakhstan'
+    ,'KENYA': 'Kenya'
+    ,'LATVIA': 'Latvia'
+    ,'LITHUANIA': 'Lithuania'
+    ,'MALAYSIA': 'Malaysia'
+    ,'MEXICO': 'Mexico'
+    ,'MOROCCO': 'Morocco'
+    ,'NEW ZEALAND': 'New Zealand'
+    ,'NICARAGUA': 'Nicaragua'
+    ,'NORWAY': 'Norway'
+    ,'PANAMA': 'Panama'
+    ,'PARAGUAY': 'Paraguay'
+    ,'PERU': 'Peru'
+    ,'PHILIPPINES': 'Philippines'
+    ,'POLAND': 'Poland'
+    ,'ROMANIA': 'Romania'
+    ,'RUSSIA': 'Russia'
+    ,'SINGAPORE': 'Singapore'
+    ,'SO. AFRICA': 'South Africa'
+    ,'SWEDEN': 'Sweden'
+    ,'SWITZERLAND': 'Switzerland'
+    ,'TAIWAN': 'Taiwan'
+    ,'THAILAND': 'Thailand'
+    ,'TURKEY': 'Turkey'
+    ,'U.K.': 'United Kingdom'
+    ,'UKRAINE': 'Ukraine'
+    ,'URUGUAY': 'Uruguay'
+    ,'VENEZUELA': 'Venezuela'
+    ,'VIETNAM': 'Vietnam'
+}
+
+DATE_COLUMN = 'DATE'
+RAW_COUNTRY_COLUMN='COUNTRY'
+HARMONIZED_COUNTRY_COLUMN='HARMONIZED_COUNTRY'
+FX_RATES_COLUMN = 'FX_Rates'
+
+USD_FX_Rate = 1.0
+EURO_CURRENCY_NAME = 'Euro'
+
+COUNTRIES_THAT_USE_USD = {
+    'Bahrain', 'Kuwait', 'Oman', 'Pan Arab', 'Pan Asian', 'Puerto Rico',
+    'Qatar', 'Saudi Arabia', 'United Arab Emirates', 'USA'
+}
+
+COUNTRIES_THAT_USE_EURO = {
+    'Austria', 'Belgium', 'Finland', 'France', 'Germany', 'Greece',
+    'Ireland', 'Italy', 'Netherlands', 'Portugal', 'Slovakia', 'Spain'
+}
