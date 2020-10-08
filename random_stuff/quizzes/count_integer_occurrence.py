9e48822b9da9f1e24c62f7f6f34677bbc2ccaddd
plartoo <plartoo@users.noreply.github.com>
2020-10-08 11:04:09 -0400

    One of the experiment code files.

--- random_stuff/quizzes/count_integer_occurrence.py
+++ random_stuff/quizzes/count_integer_occurrence.py
@@ -1,15 +1,44 @@
 # https://stackoverflow.com/q/62124898/1330974
 
-import json
+# import json
+#
+# counting_table = dict(zip([str(i) for i in range(10)],[0 for i in range(10)]))
+#
+# compute_counter = 0
+# n = 123456 # 1,000,000 == 5,888,896
+# print(f"n: {n}")
+# for num in range(1, n+1):
+#     for digit in str(num):
+#         compute_counter += 1
+#         counting_table[digit] += 1
+#
+# print(json.dumps(counting_table, sort_keys=True, indent=4))
+# print(compute_counter)
 
-counting_table = dict(zip([str(i) for i in range(10)],[0 for i in range(10)]))
 
-compute_counter = 0
-n = 1000 # 1,000,000 == 5,888,896
-for num in range(1, n+1):
-    for digit in str(num):
-        compute_counter += 1
-        counting_table[digit] += 1
+def main():
+    n = int(input('n : '))
+    num_arr = [0] * 10
+    w = 1
 
-print(json.dumps(counting_table, sort_keys=True, indent=4))
-print(compute_counter)
+    for step in range(len(str(n))):
+        remaining = 9 - int(str(n)[-1:])
+
+        for i in range(len(num_arr)):
+            num_arr[i] += (n // 10 + 1) * w
+
+        for i in range(10 - remaining, 10):
+            num_arr[i] -= w
+        num_arr[0] -= w
+
+        for number in str(n)[:-1]:
+            num_arr[int(number)] -= remaining * w
+
+        n //= 10
+        w *= 10
+
+    print(num_arr)
+
+
+if __name__ == '__main__':
+    main()
