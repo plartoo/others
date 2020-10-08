4d72501629f423a2233c2a7f724cd3c4cce90348
Maik8080 <maicol.contreras@groupm.com>
2020-10-08 09:03:51 -0500

    It was added a new function in order to validate the category dictonary per country and the categories that we use in constants file

--- data_architect_misc/data_transformer/transform_functions/common_comp_harm_transform_functions.py
+++ data_architect_misc/data_transformer/transform_functions/common_comp_harm_transform_functions.py
@@ -560,6 +560,26 @@ class CommonCompHarmTransformFunctions(CommonTransformFunctions, CommonCompHarmQ
             [comp_harm_constants.GROSS_SPEND_COLUMN],
             2)
 
+    def add_HARMONIZED_CATEGORY_column_using_existing_category_column_with_country_specific_mappings(
+            self,
+            df,
+            dict_country_specific_categories,
+            existing_category_col_name: str):
+        """
+        We have some specific category mappings in different countries, so we will
+        wrap the common comp. harm. transform function with this one.
+        """
+        # REF: https://stackoverflow.com/a/1784128/1330974
+        category_mappings = dict(comp_harm_constants.CATEGORY_MAPPINGS,
+                                 **dict_country_specific_categories)
+
+        return self.add_new_column_with_values_based_on_another_column_values_using_regex_match \
+            (df,
+             existing_category_col_name,
+             comp_harm_constants.CATEGORY_COLUMN,
+             category_mappings
+             )
+
     def add_HARMONIZED_CATEGORY_column_using_existing_category_column(
             self,
             df,
