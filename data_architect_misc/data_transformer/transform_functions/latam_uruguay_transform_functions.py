14f01782aae54ce0a667ccacb41d58b869291334
Maik8080 <maicol.contreras@groupm.com>
2020-11-19 16:59:24 -0500

    Some config files were deleted and renamed since we are using only the all data and we don't need to use daypart and network information

--- data_architect_misc/data_transformer/transform_functions/latam_uruguay_transform_functions.py
+++ data_architect_misc/data_transformer/transform_functions/latam_uruguay_transform_functions.py
@@ -26,22 +26,6 @@ class LatamUruguayTransformFunctions(CommonCompHarmTransformFunctions):
     def __init__(self, config):
         self.config = config
 
-    def create_the_date_column_using_the_header_inside_of_the_file(self,
-                                                   df,
-                                                   leave_empty_if_no_match = False
-                                                   ):
-        """
-        Specific function in order to get the date of the data inserted in the header inside of the file.
-        """
-        dt_temp = pd.read_excel(self.config['current_input_file'])
-        get_date = dt_temp[dt_temp.iloc[:,0].str.contains('Nombre: ')==True].dropna(axis=1).iloc[:,0].tolist()
-        month_year_string = [date for date in re.split(r':',get_date[0]) if 'desde' in date or 'hasta' in date][0]
-        string_to_delete = re.match(r'.*hasta*.',[date for date in re.split(r':',get_date[0]) if 'desde' in date or 'hasta' in date][0]).group()
-        month_year = month_year_string.replace(string_to_delete,'')
-        df['MES'] = month_year
-        
-        return df
-
     def apply_country_specific_category_mapping_to_HARMONIZED_CATEGORY_column(self,
                                                    df,
                                                    existing_category_col_name: str,
