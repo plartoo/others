9781bcb3cd66445bbc79248619164e9cd10116f7
Maik8080 <maicol.contreras@groupm.com>
2020-11-19 14:18:03 -0500

    new transform module for Paraguay

---
+++ data_architect_misc/data_transformer/transform_functions/latam_paraguay_transform_functions.py
@@ -0,0 +1,42 @@
+"""This is the subclass of Transform function for Paraguay (LATAM division).
+
+We will define transform functions specific to Paraguay here.
+
+Author: Maicol Contreras
+Last Modified: September 9, 2020
+"""
+
+import pandas as pd
+
+from constants import comp_harm_constants
+from transform_functions.common_comp_harm_transform_functions import CommonCompHarmTransformFunctions
+
+
+class LatamParaguayTransformFunctions(CommonCompHarmTransformFunctions):
+    """
+    All custom (uncommon) transform functions **SPECIFIC to
+    individual processing task** must be defined as part
+    of this class.
+    """
+    PARAGUAY_SPECIFIC_CATEGORY_MAPPINGS = {
+        "(?i).*TRAPEADOR.*": "Home Care",
+    }
+
+    def __init__(self, config):
+        self.config = config
+
+    def apply_country_specific_category_mapping_to_HARMONIZED_CATEGORY_column(self,
+                                                   df,
+                                                   existing_category_col_name: str,
+                                                   leave_empty_if_no_match = False
+                                                   ):
+        """
+        Helper function to invoke the common comp harm function that will help us apply
+        country-specific mappings for HARMONIZED_CATEGORY column.
+        """
+        return self. add_HARMONIZED_CATEGORY_column_using_existing_category_column_with_country_specific_mappings(
+            df,
+            LatamParaguayTransformFunctions.PARAGUAY_SPECIFIC_CATEGORY_MAPPINGS,
+            existing_category_col_name,
+            leave_empty_if_no_match
+        )
