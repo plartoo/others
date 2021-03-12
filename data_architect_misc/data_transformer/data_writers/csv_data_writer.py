5fe957dbf045ad100960407a84f2bc2b51db2719
plartoo <plartoo@users.noreply.github.com>
2021-03-12 14:31:16 -0500

    Added quoting option for writing CSV output files.

--- data_architect_misc/data_transformer/data_writers/csv_data_writer.py
+++ data_architect_misc/data_transformer/data_writers/csv_data_writer.py
@@ -1,3 +1,4 @@
+import csv
 import logging
 
 from data_writers.file_data_writer import FileDataWriter
@@ -11,9 +12,18 @@ class CSVDataWriter(FileDataWriter):
 
     # Default values for  pandas' to_csv related parameters.
     # We can later implement other pandas.to_csv parameters
-    # such as na_rep, compression, quoting, quotechar, etc.
+    # such as na_rep, compression, quotechar, etc.
     KEY_OUTPUT_DELIMITER = 'output_delimiter'
     DEFAULT_OUTPUT_DELIMITER = '|'
+
+    KEY_QUOTING = 'quoting'
+    DEFAULT_QUOTING = csv.QUOTE_MINIMAL
+    QUOTING_DICT = {
+        "csv.QUOTE_ALL": csv.QUOTE_ALL,
+        "csv.QUOTE_MINIMAL": csv.QUOTE_MINIMAL,
+        "csv.QUOTE_NONNUMERIC": csv.QUOTE_NONNUMERIC,
+        "csv.QUOTE_NONE": csv.QUOTE_NONE
+    }
 
     OUTPUT_FILE_EXTENSION = '.csv'
 
@@ -23,6 +33,10 @@ class CSVDataWriter(FileDataWriter):
         self.output_csv_delimiter = config.get(
             self.KEY_OUTPUT_DELIMITER,
             self.DEFAULT_OUTPUT_DELIMITER)
+        self.quoting = CSVDataWriter.QUOTING_DICT[
+            config.get(self.KEY_QUOTING,
+                       self.DEFAULT_QUOTING)
+        ]
 
     def _get_output_file_extension(self):
         return self.OUTPUT_FILE_EXTENSION
@@ -39,5 +53,6 @@ class CSVDataWriter(FileDataWriter):
             sep=self.output_csv_delimiter,
             line_terminator='',
             index=self.include_index,
-            encoding=self.output_file_encoding
+            encoding=self.output_file_encoding,
+            quoting=self.quoting
         )
